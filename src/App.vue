a8a51032cba98560d730a43185324a3edf24de86
Xi <xi@Xis-MacBook-Pro.local>
2023-03-17 11:51:44 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <TextInput @submit="onSubmit" />
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <div v-if="submittedText || submittedImageUrl" class="my-2">
+            <div class="card">
+              <div class="card-body">
+                <div v-if="submittedTitle" class="h2 text-center font-weight-bold mb-2">
+                  {{ submittedTitle }}
+                </div>
+                <div v-if="submittedText">{{ submittedText }}</div>
+                <div v-if="submittedImageUrl" class="text-center mt-3">
+                  <img :src="submittedImageUrl" alt="Submitted image" class="img-fluid" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TextInput from "./components/TextInput.vue";
+
+export default {
+  components: {
+    TextInput,
+  },
+  data() {
+    return {
+      submittedTitle: "",
+      submittedText: "",
+      submittedImageUrl: "",
+    };
+  },
+  methods: {
+    onSubmit(apiResponse) {
+      this.submittedTitle = apiResponse.title;
+      this.submittedText = apiResponse.text;
+      this.submittedImageUrl = apiResponse.imgUrl;
+    },
+  },
+};
+</script>
---
+++ src/components/TextInput.vue
@@ -0,0 +1,55 @@
+<!-- src/components/TextInput.vue -->
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <div class="card my-3">
+          <div class="card-body">
+            <textarea
+              class="form-control"
+              placeholder="Enter text here"
+              v-model="inputText"
+              rows="5"
+            ></textarea>
+            <div class="text-center mt-3">
+              <button
+                class="btn btn-primary"
+                type="button"
+                @click="handleButtonClick"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      inputText: "",
+    };
+  },
+  methods: {
+    async handleButtonClick() {
+      try {
+        const response = await axios.post(
+          "https://bjeovandxk.execute-api.us-west-2.amazonaws.com/dev1",
+          {
+            text: this.inputText,
+          }
+        );
+        this.$emit("submit", response.data);
+      } catch (error) {
+        console.error("API request failed:", error);
+      }
+    },
+  },
+};
+</script>
